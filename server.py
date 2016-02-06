0e733e8476c82a33bb16fd9553201d8d4aa80b69
aldilaff <aldilaff@gmail.com>
2016-02-06 13:30:58 -0600

    We have sockets now. added .gitignore and requirements.txt

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+@app.route('/testingsockets')
+def testingsockets():
+    return render_template('testingsockets.html')
+
+
+@socketio.on('my event')
+def handle_message(message):
+    print('received message: ' + str(message))
+
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
