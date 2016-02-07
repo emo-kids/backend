44e63949dc7fda7e1a5099b7f2249c14bf98fe14
aldilaff <aldilaff@gmail.com>
2016-02-06 21:47:05 -0500

    sends avg over socket

--- server.py
+++ server.py
@@ -16,33 +16,23 @@ def hello_world():
 def vis():
     return render_template('vis.html')
 
-@app.route('/avg')
-def avg():
-	if len(recent_history) > 0:
-		number_of_people = len(recent_history)
-		final_sums = [0,0,0,0]
-		for key in recent_history:
-			for index, emotion in enumerate(recent_history[key]):
-				final_sums[index] += emotion
-		avg = []
-		for sums in final_sums:
-			print(sums/number_of_people)
-			avg.append(sums/number_of_people)
-			print("a ok")
-		# print(avg)
-		json = {'averages': avg}
-		return jsonify(**json)
-		# return "hi"
-	else:
-		dictonary = {"error":"length of recent_history is < 0"}
-		return(jsonify(**dictonary))
+average_result = {}
 
 
 @socketio.on('emotions')
 def handle_emotions(emotions):
-	print(request.sid, emotions)
+	# print(request.sid, emotions)
 	recent_history[request.sid] = emotions
-	emit("emotions", [request.sid] + emotions, namespace='/vis', broadcast=True)
+	number_of_people = len(recent_history)
+	final_sums = [0,0,0,0]
+	for key in recent_history:
+		for index, emotion in enumerate(recent_history[key]):
+			final_sums[index] += emotion
+	avg = []
+	for sums in final_sums:
+		avg.append(sums/number_of_people)
+	emit("emotions", {'user': request.sid, 'count':len(recent_history), 'emotions': emotions, 'averages': avg}, namespace='/vis', broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app)
