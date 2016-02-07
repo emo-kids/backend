482531ff76bf38a613e0483c6bb027b93fde2394
aldilaff <aldilaff@gmail.com>
2016-02-06 22:41:23 -0500

    sockets now have rgb scale emotions

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit, send
 from operator import add
-import MySQLdb
+from math import sqrt
+# import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="emokids", db="emokids")
-cursor = db.cursor()
+# db = MySQLdb.connect(host="localhost", user="root", passwd="emokids", db="emokids")
+# cursor = db.cursor()
 
 recent_history = {}
 
@@ -23,6 +24,14 @@ def vis():
 
 average_result = {}
 
+def make_rgb_scale(emotion):
+	rgb = ((255*emotion)/1-sqrt(emotion))
+	if(rgb > 255):
+		return 255
+	elif(rgb < 0):
+		return 0
+	else:
+		return rgb
 
 @socketio.on('emotions')
 def handle_emotions(emotions):
@@ -31,15 +40,17 @@ def handle_emotions(emotions):
     number_of_people = len(recent_history)
     final_sums = [0,0,0,0]
     for key in recent_history:
-		  for index, emotion in enumerate(recent_history[key]):
-        final_sums[index] += emotion
+    	for index, emotion in enumerate(recent_history[key]):
+			final_sums[index] += emotion
     avg = []
     for sums in final_sums:
 		  avg.append(sums/number_of_people)
-
-    emit("emotions", {'user': request.sid, 'count':len(recent_history), 'emotions': emotions, 'averages': avg}, namespace='/vis', broadcast=True)
-    cursor.execute("INSERT INTO emotions VALUES (NULL, %1.8f, %1.8f, %1.8f, %1.8f, NOW())", emotions(0), emotions(1), emotions(2), emotions(3))
-    db.commit()
+    emotions_RGB_scale = []
+    for emotion in emotions:
+		emotions_RGB_scale.append(make_rgb_scale(emotion))
+    emit("emotions", {'user': request.sid, 'count':len(recent_history), 'emotions': emotions, 'averages': avg, 'rgb_scale':emotions_RGB_scale}, namespace='/vis', broadcast=True)
+    # cursor.execute("INSERT INTO emotions VALUES (NULL, %1.8f, %1.8f, %1.8f, %1.8f, NOW())", emotions(0), emotions(1), emotions(2), emotions(3))
+    # db.commit()
 
 if __name__ == '__main__':
     socketio.run(app)
